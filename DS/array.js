8bf6f4df8cd7864d20acad5afa6f438da6f69e53
Shoaib7889 <shoaib4030891@gmail.com>
2022-02-15 21:50:01 +0500

    sliding window technique

---
+++ DS/array.js
@@ -0,0 +1,24 @@
+const maxSubArray = (arr, size) => {
+  let currSum = 0;
+  let maxSum = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    currSum += arr[i];
+    if (i >= size - 1) {
+      maxSum = Math.max(maxSum, currSum);
+      currSum -= arr[i - (size - 1)];
+    }
+  }
+  return maxSum;
+};
+let arr = [1, 10, 3, 4, 5];
+cl(maxSubArray(arr, 2));
+
+/*
+
+cl func
+
+*/
+function cl(params) {
+  console.log(params);
+}
